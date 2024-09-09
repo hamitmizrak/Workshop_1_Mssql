c30a5b2498c550f2b7d9421b28356a3d7e895ae4
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-09 11:40:25 +0300

    export database

---
+++ workshop_1_mssql.sql
@@ -0,0 +1,55 @@
+﻿-- Single Commet
+/*
+multiple commet
+*/
+
+-- İlişkisel Veritabanı (RDBMS)
+-- İlişkisel Olmayan veritabanı (NoSql)
+
+
+-- İlişkisel Veritabanı (RDBMS): Mssql
+/*
+Veritabanı: 
+- Organize edilmiş ve yapılandırılmış veri koleksiyonlarına denir.
+- Veriyi düzenli,erişebilirliğini sağlayan, veri tekrarını önleyecek şekilde hazırlanmalıdır. (1NF,2NF,BCND)
+*/
+
+/*
+Veritabanı Bileşenleri 
+- Veri
+- Tablolar
+- Satırlar(rows)
+- Sutunlar(cols)
+- İlişkiler
+- Anahtar 
+  - Birincil Anahtar(Primary Key): Her tabloda mutlaka PK vardır ve kayıt() için benzersiz(unique) olmalıdır.
+  - Yabancı Anahtar(Foreign Key): Başka tabloda bulunan PK'dir.
+*/
+
+/*
+Veritabanı Genel Kuralları (common rules) 
+- sutun adlarını hepsi küçük ve yeni kelimeler arasında alt çizgi(_) exam: category_name
+- Sutun adlarını isim,sıfat olarak kullananın.
+*/
+
+/*
+İlişkisel Veritabanları
+- Mysql
+- Postgresql
+- Mssql
+- Oracle
+- H2DB
+- Oracle
+- MariaDB
+*/
+
+
+/*
+İlişkisel Olmayan Veritabanları
+- MongoDB
+- Cassandra
+- Redis
+- Neo4j
+*/
+
+use nortwind;

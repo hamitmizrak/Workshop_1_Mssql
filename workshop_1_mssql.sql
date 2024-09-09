6f4e2f2eaf3753fa1f3f9d8613b66dd25b3c036d
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-09 11:49:01 +0300

    database özellikleri

--- workshop_1_mssql.sql
+++ workshop_1_mssql.sql
@@ -27,13 +27,22 @@ Veritabanı Bileşenleri
 */
 
 /*
+Veritabanı Türleri
+- Hiyerarşik Veritabanları (IBM Information Management System)
+- Ağ Veritabanları
+*/
+
+
+/*
 Veritabanı Genel Kuralları (common rules) 
 - sutun adlarını hepsi küçük ve yeni kelimeler arasında alt çizgi(_) exam: category_name
 - Sutun adlarını isim,sıfat olarak kullananın.
 */
 
+
 /*
-İlişkisel Veritabanları
+İlişkisel Veritabanları (RDBMS)
+NOT: Aşağıdaki RDBMS'ler Veritabanlarını oluşturmak, yönetmek, manipüle etmek(Delete,Update,Create)
 - Mysql
 - Postgresql
 - Mssql
@@ -51,5 +60,61 @@ Veritabanı Genel Kuralları (common rules)
 - Redis
 - Neo4j
 */
+
+/*
+İlişkisel Veritabanları (RDBMS) Başlıca İşlevleri
+Veri tanımlama
+Veri manipulasyonu
+Veri Güvenliği
+Veri Yedekleme, Geri Yükleme
+Çoklu Kullanıcılı Erişim
+*/
+
+/*
+Veritabanı Avantajları
+Veri tutarlılığını
+Veri Paylaşımı
+Veri Güvenliği
+Veri Yedekleme, Geri Yükleme
+Veri Entegrasyonu
+*/
+
+
+/*
+Mysql 
+Geliştirici: Oracle
+Platform:  Windows, Linux, MacOs
+Kullanım Alanı: Küçük ve Orta ölçeklidir.
+Mysql  Özelliği:
+	- Basit Kurulumu
+	- Geniş eklenti desteği InnoDB, MyISAM
+	- Yüksek hızda veri yazma okuma
+	- Performansı iyidir.
+*/
+
+/*
+Postgresql 
+Geliştirici: PostgreSQL Global Development Group
+Platform:  Windows, Linux, MacOs
+Kullanım Alanı: Küçük,Orta,Büyük ölçeklidir.
+Postgresql  Özelllikleri:
+	- Ver türleri çok zengin
+	- JSON, XML veri türlerini dahi destekler.
+*/
+
+/*
+Mssql (Microsoft SQL Server)
+Geliştirici: Microsoft
+Platform:  Windows, Linux
+Kullanım Alanı: Küçük,Orta,Büyük ölçeklidir.
+Mssql (Microsoft SQL Server) Özellikleri
+- Zengin yönetim araçları SSMS
+- Güçlü veri bütünlüğünü sağlar.
+- Veri yedekleme, kurtama eşsizdir.
+- Performans
+*/
+
+
+
 
 use nortwind;

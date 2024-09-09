7f829c8e8c6c4bf1d2415f79522801afded898fa
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-09 12:18:38 +0300

    Mssql Syntax

--- workshop_1_mssql.sql
+++ workshop_1_mssql.sql
@@ -6,7 +6,6 @@ multiple commet
 -- İlişkisel Veritabanı (RDBMS)
 -- İlişkisel Olmayan veritabanı (NoSql)
 
-
 -- İlişkisel Veritabanı (RDBMS): Mssql
 /*
 Veritabanı: 
@@ -127,7 +126,16 @@ Dezavantaj:
     - Sınırlı ölçeklendirme
 */
 
-
+/*
+~~~~~Mssql Syntax ~~~~~
+SELECT *, Distinct(Tekrarsız Veriler), Top(istenilen sayıda Listeleme), Min,Max,Avg(Ortalama),Sum, Count
+FROM `databaseAdi`.`tabloAdi`
+JOIN (Birden fazla tabloda ortak vb yapıları listelemek)
+WHERE (Şart, BIL) (Between, In, Like)
+GROUP BY (Belli kolon için gruplama yapmak içindir)
+HAVING (Filtreleme) (Sum, Avg, Count, Min, Max)
+ORDER BY (Sıralama)
+*/
 
 
 use nortwind;

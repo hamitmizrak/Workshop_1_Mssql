b64dd2d9b446830f06898bb8d8626c157272148e
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-11 11:44:05 +0300

    DISTINCT,OPERATOR,MATH

--- workshop_1_mssql.sql
+++ workshop_1_mssql.sql
@@ -190,7 +190,7 @@ SELECT * FROM Categories WHERE CategoryID>=3 ORDER BY CategoryID desc;
 -- 2.YOL (ALIAS)
 SELECT * FROM Categories as cat WHERE cat.CategoryID>=3 ORDER BY cat.CategoryID desc;
 
-/* TOP */
+/*~~~~ TOP: Veri Sınırla */
 -- nortwind databasesinde Categories tablosundaki verilerden sadece CategoryID'sindeki baştan 4 elemanı listeleyelim ?
 SELECT * FROM Categories;
 SELECT TOP 4 * FROM Categories;
@@ -203,8 +203,14 @@ SELECT TOP 4  * FROM Categories ORDER BY CategoryID desc;
 SELECT TOP(4) * FROM Categories  ORDER BY CategoryID desc;
 SELECT TOP(4) * FROM Categories as cat ORDER BY cat.CategoryID desc;
 
+/*~~~~ DISTINCT: Tekrarsız Veri ~~~~*/
+SELECT * FROM Customers;
+-- nortwind databasesinde `Customers` tablosundaki ContactTitle sutunundaki verileri tekrarsız olarak gösteriniz ?
+SELECT DISTINCT ContactTitle FROM Customers;
+SELECT DISTINCT cus.ContactTitle FROM Customers as cus;
+SELECT DISTINCT cus.ContactTitle FROM Customers  cus;
 
-/* SELECTOR(Seçiciler) */
+/*~~~~ SELECTOR(Seçiciler) */
 -- 1.YOL
 use nortwind;
 SELECT * FROM Categories;
@@ -214,35 +220,62 @@ SELECT * FROM [nortwind].[dbo].[Categories] as cat;
 SELECT cat.CategoryID,cat.CategoryName,cat.Description,cat.Picture FROM [nortwind].[dbo].[Categories] as cat;
 
 
-/* IS NULL: Customers */
+/*~~~~ IS NULL: null değerleri göster ~~~~*/
 -- nortwind database Customers tablosundaki `Region` sutunudaki NULL olan değerleri listeleyelim ?
 SELECT * FROM [nortwind].[dbo].Customers as cat WHERE cat.Region IS NULL;
 
 
-/* IS NOT NULL */
+/*~~~~ IS NOT NULL:  null olmayan değerleri göster ~~~~*/
 -- nortwind database Customers tablosundaki `Region` sutunudaki NULL olmayan değerleri listeleyelim ?
 SELECT * FROM [nortwind].[dbo].Customers as cat WHERE cat.Region IS NOT NULL ;
 
 
-/* OPERATOR */
+/*~~~~ OPERATOR: Topla,çıkar,çarp,böl,kalan ~~~~*/
+/* + - * / %   */
+SELECT 8+2;
+SELECT 8+2 as TOPLAM;
+SELECT 8+2 as "SONUÇ TOPLAM";
+SELECT 8+2 as 'SONUÇ TOPLAM';
+-- select 8+2 as `SONUÇ TOPLAM`; -- backtick(``) kullanma
+
+select 8-2 as 'SONUÇ ÇIKARMA';
+SELECT 8*2 as 'SONUÇ ÇARPMA';
+SELECT 8/2 as 'SONUÇ BÖLME';
+SELECT 8%2 as 'SONUÇ KALAN';
 
 
-/* MATH */
+/*~~~~ MATH : Mathematics~~~~*/
+SELECT pi() as 'PI Sayısı';
+SELECT PI() as 'PI Sayısı';
+SELECT Pi() as 'PI Sayısı';
+SELECT pI() as 'PI Sayısı';
+
+SELECT abs(-44) as 'MUTLAK';
+SELECT sqrt(16) as 'KAREKÖK';
+SELECT power(2,5) as 'ÜSLÜ SAYILAR';
+
+SELECT floor(6.9) as 'ALTA YUVARLA';
+SELECT ceiling(6.1) as 'YUKARI YUVARLA';
+
+SELECT sin(45) as 'SINUS';
+SELECT cos(45) as 'COSINUS';
+SELECT tan(45) as 'TANJANT';
+SELECT cot(45) as 'COTANJANT';
 
 
-/* AGGREGATE */
+/*~~~~ AGGREGATE ~~~~*/
 
 
-/* CAST*/
+/*~~~~ CAST ~~~~*/
 
 
-/* WHERE: BIL(Between, In, Like) */
+/*~~~~ WHERE: BIL(Between, In, Like) ~~~~*/
 
 
-/* STRING */
+/*~~~~ STRING ~~~~*/
 
 
-/* DATE */
+/*~~~~ DATE ~~~~*/
 
 /*################################################################################ */
 /*
